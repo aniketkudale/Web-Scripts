6153377c5f1c3164e495a6497d0dec7ba8a4964b
Aniket Kudale <aniket.kudale@hotmail.com>
2014-04-20 13:45:28 +0530

    Update LVisitor.js

--- LVisitor.js
+++ LVisitor.js
@@ -1,23 +1,22 @@
-/* 
- * By: Aniket Kudale
- * Email: aniket.kudale@hotmail.com
- * Javascript Function for Visiting URL Multiple Times
- * Dated: 19-04-2014
- */
+/*
+* By: Aniket Kudale
+* Email: aniket.kudale@hotmail.com
+* http://www.github.com/aniketkudale
+* Javascript Function for Visiting URL Multiple Times
+* Dated: 19-04-2014
+*/
 
 
 
-function LVisitor(event){
+function Lvisitor(event){
   
-  	var count = 0; // counting the no. of times the link has been visited.
+	var count = 0; // counting the no. of times the link has been visited.
 	var test = setInterval(function() {
-	var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"] .getService(Components.interfaces.nsIWindowMediator);
-        var mainWindow = wm.getMostRecentWindow("navigator:browser");
-	var newTabBrowser = gBrowser.getBrowserForTab(gBrowser.addTab("http://www.google.com/")); // opens new tab
+	var newTabBrowser = gBrowser.getBrowserForTab(gBrowser.addTab("URL")); // opens new tab - specify URL
 	count++; // count incrementer
-	document.getElementById("LVisitor").value = count;		// displaying the count in browser i.e on add-on toolbar
+	document.getElementById("display").value = count;	// displaying the count in browser i.e on add-on toolbar
         newTabBrowser.addEventListener("load", function () {
-        gBrowser.removeCurrentTab(); }, true); // eventlistener, which closes the tab after the page is loaded. 
+        gBrowser.removeCurrentTab(); }, true); // eventlistener, which closes the tab after the page is loaded.
 	}, 3000); // tab is opened every 3 seconds
-	
+
  }
